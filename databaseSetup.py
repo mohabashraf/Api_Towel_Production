f98828a678004d0463ff247082122268f15fc7b3
mohabashraf <mohabashraf466@gmail.com>
2018-09-09 18:05:48 +0200

    sqllite database configuration

--- databaseSetup.py
+++ databaseSetup.py
@@ -1,9 +1,15 @@
+
 import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
-
+#Mohab Ashraf Mohamed Nazmy
+#9/9/2018
+# sqllite configuration 
+Base = declarative_base()
+engine = create_engine('sqlite:////TowlProduction')
+Base.metadata_create_all(engine) 
 Base = declarative_base()
 engine = create_engine('sqlite:////TowlProduction')
 Base.metadata_create_all(engine) 
