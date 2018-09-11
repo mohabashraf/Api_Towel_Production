fed9e59978d35816954cfd3a959250284db4df83
mohabashraf <mohabashraf466@gmail.com>
2018-09-11 19:39:51 +0200

    Create towel class

--- databaseSetup.py
+++ databaseSetup.py
@@ -9,3 +9,15 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 engine = create_engine('postgresql://usr:pass@localhost:5432/sqlalchemy')
 Base.metadata_create_all(engine) 
+
+#Mohab Ashraf
+#11/9/2018
+#Create Tower Class
+class Towel(Base):
+__tablename__ = 'toweles'
+name = Column('name', String(80), nullable = False)
+id = Column(Integer, primary_key = True)
+price = Column('price', Numeric)
+description = Column('description', String(200))
+in_stock = Column('in_stock', Boolean)
+quantity = Column('quantity', Integer)
